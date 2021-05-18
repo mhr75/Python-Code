1c90ee4658be6946f577d5bb242d0054846af27d
Mahmudul Haque <49345529+mhr75@users.noreply.github.com>
2021-05-18 13:06:35 +0600

    Create TutorialCode2.py

---
+++ python_tutorial_youtube/TutorialCode2.py
@@ -0,0 +1,22 @@
+number_1 = 10
+number_2 = 52
+
+# summation of 2 numbers
+sum = number_1 + number_2
+
+#substraction of 2 numbers
+substract = number_2 - number_1
+
+#multiplication of 2 numbers
+multiplication = number_1 * number_2
+
+#divide 2 numbers
+div = number_2 / number_1 
+
+mod = number_2 % number_1
+
+print(type(sum))
+print(substract)
+print(multiplication)
+print(div)
+print(mod)
